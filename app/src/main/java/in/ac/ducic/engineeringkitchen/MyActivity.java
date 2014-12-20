6b796f392d129ad7cd7c45c062aff74a82c18b40
Sagar <kumarsagar15795@gmail.com>
2014-12-20 14:50:23 +0530

    add navigation with item click listener

--- app/src/main/java/in/ac/ducic/engineeringkitchen/MyActivity.java
+++ app/src/main/java/in/ac/ducic/engineeringkitchen/MyActivity.java
@@ -1,20 +1,16 @@
 package in.ac.ducic.engineeringkitchen;
 
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -43,7 +39,7 @@ public class MyActivity extends ActionBarActivity {
         leftDrawerList = (ListView) findViewById(R.id.left_drawer);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        navigationDrawerAdapter=new ArrayAdapter<String>( MyActivity.this, android.R.layout.simple_list_item_1, leftSliderData);
+        navigationDrawerAdapter = new ArrayAdapter<String>(MyActivity.this, android.R.layout.simple_list_item_1, leftSliderData);
         leftDrawerList.setAdapter(navigationDrawerAdapter);
     }
 
@@ -54,16 +50,19 @@ public class MyActivity extends ActionBarActivity {
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
-
+                toolbar.setTitle("Navigation Drawer");
+        //        invalidateOptionsMenu();
             }
 
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-
+                toolbar.setTitle("open");
+      //          invalidateOptionsMenu();
             }
         };
         drawerLayout.setDrawerListener(drawerToggle);
+        leftDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
     @Override
@@ -95,4 +94,20 @@ public class MyActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // Create a new fragment and specify the planet to show based on position
+        Intent x=new Intent(this,MainActivity.class);
+        startActivity(x);
+    }
+
+
 }
